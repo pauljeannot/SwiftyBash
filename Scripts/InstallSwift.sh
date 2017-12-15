d830cf3c0a91511a37840a70f71b18d3965c4875
Paul Jeannot <chewing.gum.dum@gmail.com>
2017-12-15 08:39:54 +0100

    Update InstallSwift.sh

--- Scripts/InstallSwift.sh
+++ Scripts/InstallSwift.sh
@@ -5,8 +5,8 @@ if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
 	cd ..
 	export SWIFT_VERSION=swift-4.0.2-RELEASE
     wget https://swift.org/builds/swift-4.0.2-release/ubuntu1404/${SWIFT_VERSION}/${SWIFT_VERSION}-ubuntu14.04.tar.gz
-	tar xzf $SWIFT_VERSION-ubuntu14.04.tar.gz
-	export PATH="${PWD}/${SWIFT_VERSION}-ubuntu14.04/usr/bin:${PATH}"
+	tar xzf $SWIFT_VERSION-ubuntu16.04.tar.gz
+	export PATH="${PWD}/${SWIFT_VERSION}-ubuntu16.04/usr/bin:${PATH}"
 	cd "$DIR"
 else
 	export SWIFT_VERSION=swift-4.0.2-RELEASE
