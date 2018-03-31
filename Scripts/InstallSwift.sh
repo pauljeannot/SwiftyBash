0e89469ea07ed05ce56c09f86a73c4573034f709
Paul Jeannot <paul.jeannot95@gmail.com>
2018-03-31 17:04:36 +0200

    Update InstallSwift.sh

--- Scripts/InstallSwift.sh
+++ Scripts/InstallSwift.sh
@@ -3,14 +3,14 @@
 if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
 	DIR="$(pwd)"
 	cd ..
-	export SWIFT_VERSION=swift-4.0.2-RELEASE
-    wget https://swift.org/builds/swift-4.0.2-release/ubuntu1404/${SWIFT_VERSION}/${SWIFT_VERSION}-ubuntu14.04.tar.gz
-	tar xzf $SWIFT_VERSION-ubuntu16.04.tar.gz
-	export PATH="${PWD}/${SWIFT_VERSION}-ubuntu16.04/usr/bin:${PATH}"
+	export SWIFT_VERSION=swift-4.1
+    wget https://swift.org/builds/${SWIFT_VERSION}-release/ubuntu1604/${SWIFT_VERSION}-RELEASE/${SWIFT_VERSION}-RELEASE-ubuntu16.04.tar.gz
+	tar xzf $SWIFT_VERSION-RELEASE-ubuntu16.04.tar.gz
+	export PATH="${PWD}/${SWIFT_VERSION}-RELEASE-ubuntu16.04/usr/bin:${PATH}"
 	cd "$DIR"
 else
-	export SWIFT_VERSION=swift-4.0.2-RELEASE
-    curl -O https://swift.org/builds/swift-4.0.2-release/xcode/${SWIFT_VERSION}/${SWIFT_VERSION}-osx.pkg
-	sudo installer -pkg ${SWIFT_VERSION}-osx.pkg -target /
+	export SWIFT_VERSION=swift-4.1
+    curl -O https://swift.org/builds/${SWIFT_VERSION}-release/xcode/${SWIFT_VERSION}-RELEASE/${SWIFT_VERSION}-RELEASE-osx.pkg
+	sudo installer -pkg ${SWIFT_VERSION}-RELEASE-osx.pkg -target /
 	export TOOLCHAINS=swift
 fi
